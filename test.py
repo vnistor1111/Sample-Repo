e0fe011dbfc1a739c261f766ea9789be4e12c1b0
Nistor Victor <nistorvictorgabriel@gmail.com>
2021-11-19 16:42:25 +0200

    Initial Commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print (sys.version)
+print (sys.executable)
+
+
+def greet(who_to_greet):
+    test = "Test"
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+
---
+++ test2.py
@@ -0,0 +1,2 @@
+print ("This is file 2")
+print ("100")
